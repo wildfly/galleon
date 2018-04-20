1f3200bfaaafd827a530433dddf4002b9652ea55
Alexey Loubyansky <loubyansky@gmail.com>
2018-04-20 14:48:51 +0200

    Prepare for 1.0.0.Alpha3 release

--- do.sh
+++ do.sh
@@ -58,5 +58,5 @@ if [[ -n $BUILD ]]; then
 fi
 
 if [[ -n $RUN ]]; then
-java $JAVA_OPTS "-Dorg.wildfly.logging.skipLogManagerCheck=true" -jar ./cli/target/galleon-cli-1.0.0.Alpha3-SNAPSHOT.jar
+java $JAVA_OPTS "-Dorg.wildfly.logging.skipLogManagerCheck=true" -jar ./cli/target/galleon-cli-1.0.0.Alpha3.jar
 fi
