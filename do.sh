4b0b0e6e8ec5bb37ac184042c716ba70b2ef5162
Alexey Loubyansky <olubyans@redhat.com>
2018-05-28 00:59:20 +0200

    Prepare for 1.0.0.Final release

--- do.sh
+++ do.sh
@@ -58,5 +58,5 @@ if [[ -n $BUILD ]]; then
 fi
 
 if [[ -n $RUN ]]; then
-java $JAVA_OPTS "-Dorg.wildfly.logging.skipLogManagerCheck=true" -jar ./cli/target/galleon-cli-1.0.0.Final-SNAPSHOT.jar
+java $JAVA_OPTS "-Dorg.wildfly.logging.skipLogManagerCheck=true" -jar ./cli/target/galleon-cli-1.0.0.Final.jar
 fi
