26da4f44f7c74200d1556a1b6a1505cbcfaaabe7
Alexey Loubyansky <olubyans@redhat.com>
2018-05-28 23:02:24 +0200

    [GAL-19] If a model-only config includes a feature, the feature cannot be excluded from final configs

---
+++ core/src/test/java/org/jboss/galleon/installation/configs/customsubset/ExcludeFeatureFromModelOnlyConfigTestCase.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016-2018 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.galleon.installation.configs.customsubset;
+
+import org.jboss.galleon.ArtifactCoords;
+import org.jboss.galleon.ProvisioningDescriptionException;
+import org.jboss.galleon.ProvisioningException;
+import org.jboss.galleon.ArtifactCoords.Gav;
+import org.jboss.galleon.config.ConfigModel;
+import org.jboss.galleon.config.FeatureConfig;
+import org.jboss.galleon.config.FeaturePackConfig;
+import org.jboss.galleon.config.ProvisioningConfig;
+import org.jboss.galleon.repomanager.FeaturePackRepositoryManager;
+import org.jboss.galleon.runtime.ResolvedFeatureId;
+import org.jboss.galleon.spec.FeatureId;
+import org.jboss.galleon.spec.FeatureParameterSpec;
+import org.jboss.galleon.spec.FeatureSpec;
+import org.jboss.galleon.state.ProvisionedFeaturePack;
+import org.jboss.galleon.state.ProvisionedState;
+import org.jboss.galleon.test.PmProvisionConfigTestBase;
+import org.jboss.galleon.xml.ProvisionedConfigBuilder;
+import org.jboss.galleon.xml.ProvisionedFeatureBuilder;
+
+/**
+ *
+ * @author Alexey Loubyansky
+ */
+public class ExcludeFeatureFromModelOnlyConfigTestCase extends PmProvisionConfigTestBase {
+
+    private static final Gav FP_GAV = ArtifactCoords.newGav("org.jboss.pm.test", "fp1", "1.0.0.Final");
+
+    @Override
+    protected void setupRepo(FeaturePackRepositoryManager repoManager) throws ProvisioningDescriptionException {
+        repoManager.installer()
+        .newFeaturePack(FP_GAV)
+            .addSpec(FeatureSpec.builder("specA")
+                    .addParam(FeatureParameterSpec.createId("name"))
+                    .addParam(FeatureParameterSpec.create("p1", true))
+                    .build())
+            .addConfig(ConfigModel.builder().setModel("config1")
+                    .addFeature(new FeatureConfig().setSpecName("specA")
+                            .setParam("name", "a1")
+                            .setParam("p1", "config1"))
+                    .build())
+            .getInstaller()
+        .install();
+    }
+
+    @Override
+    protected ProvisioningConfig provisioningConfig() throws ProvisioningDescriptionException {
+        return ProvisioningConfig.builder()
+                .addFeaturePackDep(FeaturePackConfig.forGav(FP_GAV))
+                .addConfig(ConfigModel.builder().setModel("config1").setName("name1")
+                    .addFeature(new FeatureConfig().setSpecName("specA")
+                            .setParam("name", "a2")
+                            .setParam("p1", "config1"))
+                    .excludeFeature(FeatureId.create("specA", "name", "a1"))
+                    .build())
+                .build();
+    }
+
+    @Override
+    protected ProvisionedState provisionedState() throws ProvisioningException {
+        return ProvisionedState.builder()
+                .addFeaturePack(ProvisionedFeaturePack.forGav(FP_GAV))
+                .addConfig(ProvisionedConfigBuilder.builder()
+                        .setName("name1")
+                        .setModel("config1")
+                        .addFeature(ProvisionedFeatureBuilder.builder(ResolvedFeatureId.create(FP_GAV, "specA", "name", "a2"))
+                                .setConfigParam("p1", "config1")
+                                .build())
+                        .build())
+                .build();
+    }
+}
