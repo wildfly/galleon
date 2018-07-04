33b0ab9701480232498cc8609af725701f70d03a
Alexey Loubyansky <loubyansky@gmail.com>
2018-07-04 19:22:29 +0200

    next is 2.0.0.Alpha3

--- do.sh
+++ do.sh
@@ -58,5 +58,5 @@ if [[ -n $BUILD ]]; then
 fi
 
 if [[ -n $RUN ]]; then
-java $JAVA_OPTS "-Dorg.wildfly.logging.skipLogManagerCheck=true" -jar ./cli/target/galleon-cli-2.0.0.Alpha2.jar
+java $JAVA_OPTS "-Dorg.wildfly.logging.skipLogManagerCheck=true" -jar ./cli/target/galleon-cli-2.0.0.Alpha3-SNAPSHOT.jar
 fi
