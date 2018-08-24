6673739b8dcc7a5e0d174a08ffd336bcbf3d9a80
Alexey Loubyansky <loubyansky@gmail.com>
2018-08-24 18:36:54 +0200

    Prepare for 2.0.0.Final release

--- do.sh
+++ do.sh
@@ -58,5 +58,5 @@ if [[ -n $BUILD ]]; then
 fi
 
 if [[ -n $RUN ]]; then
-java $JAVA_OPTS -jar ./cli/target/galleon-cli-2.0.0.Beta2-SNAPSHOT.jar
+java $JAVA_OPTS -jar ./cli/target/galleon-cli-2.0.0.Final.jar
 fi
