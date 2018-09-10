c49895ac05ef60fc444276fd478700a92e123144
Jean-Francois Denise <jfdenise@gmail.com>
2018-09-10 17:03:35 +0200

    Fix for GAL-138, CLI tracker cursor location and no ANSI terminal

--- cli/src/main/java/org/jboss/galleon/cli/tracking/CliProgressTracker.java
+++ cli/src/main/java/org/jboss/galleon/cli/tracking/CliProgressTracker.java
@@ -16,6 +16,7 @@
  */
 package org.jboss.galleon.cli.tracking;
 
+import org.aesh.readline.terminal.impl.WinSysTerminal;
 import org.aesh.utils.ANSI;
 import org.aesh.utils.Config;
 import org.jboss.galleon.cli.PmCommandInvocation;
@@ -28,13 +29,101 @@ import org.jboss.galleon.progresstracking.ProgressTracker;
  */
 abstract class CliProgressTracker<T> implements ProgressCallback<T> {
 
+    private interface Printer {
+
+        void starting();
+
+        void processing(String content);
+
+        void complete(String content);
+    }
+
+    private class BasicPrinter implements Printer {
+
+        @Override
+        public void complete(String content) {
+            if (content != null) {
+                invocation.println(content);
+            }
+        }
+
+        @Override
+        public void starting() {
+            invocation.println(msgStart + "...");
+        }
+
+        @Override
+        public void processing(String content) {
+        }
+    }
+
+    private class ANSIPrinter implements Printer {
+
+        void print(String content, boolean moveUp) {
+            // The content is printed one line above cursor.
+            if (moveUp) {
+                invocation.getShell().write(ANSI.CURSOR_HIDE);
+                moveCursorUp();
+            }
+            invocation.getShell().write(ANSI.ERASE_WHOLE_LINE);
+            invocation.getShell().write(content);
+            if (moveUp) {
+                // Put the cursor back to where it was.
+                invocation.getShell().write(ANSI.CURSOR_RESTORE);
+                invocation.getShell().write(ANSI.CURSOR_SHOW);
+                invocation.getShell().write(ANSI.ERASE_WHOLE_LINE);
+            }
+        }
+
+        @Override
+        public void complete(String content) {
+            if (content != null) {
+                print(content + Config.getLineSeparator(), true);
+            } else {
+                invocation.getShell().write(ANSI.ERASE_WHOLE_LINE);
+            }
+        }
+
+        private void moveCursorUp() {
+            int[] out = new int[4];
+            out[0] = 27; // esc
+            out[1] = '['; // [
+            out[2] = 1;
+            out[3] = 'A';
+            invocation.getShell().write(out);
+        }
+
+        @Override
+        public void starting() {
+            // Print the start message and a new line to locate the cursor.
+            invocation.getShell().write(ANSI.CURSOR_SAVE);
+            print(msgStart + Config.getLineSeparator(), false);
+            // Save the cursor at the begining of the newly added empty line.
+            invocation.getShell().write(ANSI.CURSOR_SAVE);
+        }
+
+        @Override
+        public void processing(String content) {
+            print(content, true);
+        }
+    }
     final String msgStart;
     final String msgComplete;
     PmCommandInvocation invocation;
+    private final Printer printer;
 
     CliProgressTracker(String msgStart, String msgComplete) {
         this.msgStart = msgStart;
         this.msgComplete = msgComplete;
+        if (Config.isWindows()) {
+            if (WinSysTerminal.isVTSupported()) {
+                printer = new ANSIPrinter();
+            } else {
+                printer = new BasicPrinter();
+            }
+        } else {
+            printer = new ANSIPrinter();
+        }
     }
 
     void commandStart(PmCommandInvocation invocation) {
@@ -45,16 +134,9 @@ abstract class CliProgressTracker<T> implements ProgressCallback<T> {
         this.invocation = invocation;
     }
 
-    void print(String content) {
-        invocation.getShell().write(ANSI.CURSOR_SAVE);
-        invocation.getShell().write(ANSI.ERASE_WHOLE_LINE);
-        invocation.getShell().write(content);
-        invocation.getShell().write(ANSI.CURSOR_RESTORE);
-    }
-
     @Override
     public void starting(ProgressTracker<T> tracker) {
-        print(msgStart);
+        printer.starting();
     }
 
     // each time a new item is processed let's display it.
@@ -63,7 +145,7 @@ abstract class CliProgressTracker<T> implements ProgressCallback<T> {
     public void processing(ProgressTracker<T> tracker) {
         String content = processingContent(tracker);
         if (content != null) {
-            print(msgStart + " " + content);
+            printer.processing(msgStart + " " + content);
         }
     }
 
@@ -74,14 +156,12 @@ abstract class CliProgressTracker<T> implements ProgressCallback<T> {
 
     @Override
     public void complete(ProgressTracker<T> tracker) {
-        if (msgComplete != null) {
-            String content = completeContent(tracker);
-            print(msgComplete + (content == null ? "" : " " + content) + Config.getLineSeparator());
-        } else {
-            // Simply erase the whole line, some content will come next
-            // and will re-use the current line to print some content.
-            invocation.getShell().write(ANSI.ERASE_WHOLE_LINE);
+        String content = msgComplete;
+        if (content != null) {
+            String completedContent = completeContent(tracker);
+            content = msgComplete + (completedContent == null ? "" : " " + completedContent);
         }
+        printer.complete(content);
     }
 
     protected abstract String processingContent(ProgressTracker<T> tracker);
