83f4d0d181776cd2286b3f3fd506b13e0060fe81
Alexey Loubyansky <loubyansky@gmail.com>
2018-10-04 13:53:09 +0200

    [GAL-175] Maven plugin mojos are not closing ProvisioningManager

--- maven-plugin/src/main/java/org/jboss/galleon/maven/plugin/util/FeaturePackInstaller.java
+++ maven-plugin/src/main/java/org/jboss/galleon/maven/plugin/util/FeaturePackInstaller.java
@@ -129,8 +129,10 @@ public class FeaturePackInstaller {
     }
 
     public void install() {
-        try {
-            ProvisioningManager manager = getManager();
+        try (ProvisioningManager manager = ProvisioningManager.builder()
+                .addArtifactResolver(FeaturePackRepositoryManager.newInstance(repoHome))
+                .setInstallationHome(installationDir)
+                .build()) {
             System.setProperty("org.wildfly.logging.skipLogManagerCheck", "true");
             ConfigModel config = null;
             if (customConfig != null && Files.exists(customConfig)) {
@@ -171,12 +173,5 @@ public class FeaturePackInstaller {
         } finally {
             System.clearProperty("org.wildfly.logging.skipLogManagerCheck");
         }
-    }
-
-    private ProvisioningManager getManager() throws ProvisioningException {
-        return ProvisioningManager.builder()
-                .addArtifactResolver(FeaturePackRepositoryManager.newInstance(repoHome))
-                .setInstallationHome(installationDir)
-                .build();
     }
 }
