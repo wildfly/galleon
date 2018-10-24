6190d5e83d8ae1c0a52c20cbed932b421f24897a
Alexey Loubyansky <olubyans@redhat.com>
2018-10-24 08:12:22 +0200

    [GAL-187] CLI should inform end user that there are .glnew that needs attention

---
+++ testsuite/src/test/java/org/jboss/galleon/userchanges/test/UserChangesDirToFileTestCase.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2016-2018 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.galleon.userchanges.test;
+
+import org.jboss.galleon.ProvisioningDescriptionException;
+import org.jboss.galleon.ProvisioningException;
+import org.jboss.galleon.ProvisioningManager;
+import org.jboss.galleon.config.FeaturePackConfig;
+import org.jboss.galleon.config.ProvisioningConfig;
+import org.jboss.galleon.creator.FeaturePackCreator;
+import org.jboss.galleon.state.ProvisionedFeaturePack;
+import org.jboss.galleon.state.ProvisionedState;
+import org.jboss.galleon.test.util.fs.state.DirState;
+import org.jboss.galleon.universe.FeaturePackLocation;
+import org.jboss.galleon.universe.MvnUniverse;
+
+/**
+ *
+ * @author Alexey Loubyansky
+ */
+public class UserChangesDirToFileTestCase extends UserChangesTestBase {
+
+    private FeaturePackLocation prod1;
+
+    @Override
+    protected void createProducers(MvnUniverse universe) throws ProvisioningException {
+        universe.createProducer("prod1");
+    }
+
+    @Override
+    protected void createFeaturePacks(FeaturePackCreator creator) throws ProvisioningException {
+        prod1 = newFpl("prod1", "1", "1.0.0.Final");
+        creator.newFeaturePack(prod1.getFPID())
+            .newPackage("p1", true)
+                .writeContent("a/test.txt", "prod1");
+
+        creator.install();
+    }
+
+    @Override
+    protected void testPm(ProvisioningManager pm) throws ProvisioningException {
+        pm.install(prod1);
+        recursiveDelete("a");
+        writeContent("a", "test");
+        pm.install(prod1);
+    }
+
+
+    @Override
+    protected ProvisioningConfig provisionedConfig() throws ProvisioningDescriptionException {
+        return ProvisioningConfig.builder()
+                .addFeaturePackDep(FeaturePackConfig.builder(prod1)
+                        .build())
+                .build();
+    }
+
+    @Override
+    protected ProvisionedState provisionedState() throws ProvisioningException {
+        return ProvisionedState.builder()
+                .addFeaturePack(ProvisionedFeaturePack.builder(prod1.getFPID())
+                        .addPackage("p1")
+                        .build())
+                .build();
+    }
+
+    @Override
+    protected DirState provisionedHomeDir() {
+        return newDirBuilder()
+                .addFile("a", "test")
+                .build();
+    }
+}
