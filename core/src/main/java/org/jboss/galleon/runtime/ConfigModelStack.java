3d17d0c93a4a5f55a19c78bded35d0e589ddb7bc
Alexey Loubyansky <loubyansky@gmail.com>
2019-04-05 18:57:59 +0200

    [GAL-268] Layers included in model.xml don't appear in provisioned.xml

--- core/src/main/java/org/jboss/galleon/runtime/ConfigModelStack.java
+++ core/src/main/java/org/jboss/galleon/runtime/ConfigModelStack.java
@@ -423,6 +423,13 @@ class ConfigModelStack {
                 }
             }
         }
+        if(other.includedLayers != null && !other.includedLayers.isEmpty()) {
+            for(ConfigId layerId : other.includedLayers) {
+                if(addLayer(layerId)) {
+                    includedLayer(layerId);
+                }
+            }
+        }
 
         if(other.specFeatures.isEmpty()) {
             return;
