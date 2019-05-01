3017846993ae155562a18e4984bdeb9ec6e5da32
Alexey Loubyansky <olubyans@redhat.com>
2019-05-01 23:48:00 +0200

    update license headers

--- core/src/test/java/org/jboss/galleon/featurepack/location/test/FeaturePackLocationStringTestCase.java
+++ core/src/test/java/org/jboss/galleon/featurepack/location/test/FeaturePackLocationStringTestCase.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jboss.galleon.featurepack.location.test;
 
 import org.jboss.galleon.universe.FeaturePackLocation;
